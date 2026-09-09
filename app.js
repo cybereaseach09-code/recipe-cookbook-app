ea796a066e77592147abc32e896f31604bb0b0a0
cybereaseach09-code <cybereaseach09@gmail.com>
2026-09-09 20:15:34 +0800

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_URL = "YOUR_FUNCTION_URL_HERE";
+const API_URL = "https://photo-inspector-api-ajbth9dmf0adduaq.australiaeast-01.azurewebsites.net/api/AnalyzeImage";
 
 async function searchRecipes() {
     const query = document.getElementById("searchInput").value.toLowerCase();
